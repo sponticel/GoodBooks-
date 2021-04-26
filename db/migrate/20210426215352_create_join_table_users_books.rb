47c81fdc8947a4a16bcc58eaa299dea44e1d4f56
Sandy Ponticel <sponticel@gmail.com>
2021-04-26 18:06:53 -0400

    made tables no migrate

---
+++ db/migrate/20210426215352_create_join_table_users_books.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableUsersBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_join_table :users, :books do |t|
+      # t.index [:user_id, :book_id]
+      # t.index [:book_id, :user_id]
+    end
+  end
+end
