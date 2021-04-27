061198a8669ff9e3e9a6b7b90a1c5a45c4c60334
Sandy Ponticel <sponticel@gmail.com>
2021-04-27 19:32:01 -0400

    seeded succesfully and started authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :reviews
-  has_many :books, optional: true
+  has_many :books
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }
 end
