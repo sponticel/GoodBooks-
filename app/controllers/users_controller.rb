46733137a8f0e1c430baad4f6aaf882f00c6e3e6
Sandy Ponticel <sponticel@gmail.com>
2021-04-27 20:08:02 -0400

    cleaned up users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
-
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
-  end
 
   # POST /users
   def create
@@ -19,7 +6,6 @@ class UsersController < ApplicationController
 
     if @user.save
       @token = encode({ id: @user.id })
-
       render json: {
         user: @user.attribute.except('password_digest'),
         token: @token
@@ -28,27 +14,9 @@ class UsersController < ApplicationController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
-
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
-
+  
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
+    
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:username, :email, :password)
