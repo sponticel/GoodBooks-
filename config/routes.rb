a18cc637551bb157389a139e0f3dd087448140be
Sandy Ponticel <sponticel@gmail.com>
2021-04-28 14:15:24 -0400

    set up api-helper

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
   resources :reviews, only: [:index, :create, :update, :destroy]
-  resources :books ,  only: :index
+  resources :books ,  only: [:index, :show]
   resources :users, only: [:create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
