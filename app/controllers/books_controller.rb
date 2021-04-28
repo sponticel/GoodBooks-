bf2cdd471b2f932676fe64692dcdf2ed68ebf757
Sandy Ponticel <sponticel@gmail.com>
2021-04-28 11:29:06 -0400

    routes working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :authorize_request, only: [:create, :update, :destroy]
+  # before_action :authorize_request, only: [:create, :update, :destroy]
   before_action :set_book, only: [:show, :update, :destroy]
 
   # GET /books
@@ -11,33 +11,33 @@ class BooksController < ApplicationController
 
   # GET /books/1
   def show
-    render json: @book
+    render json: @book, include: :reviews
   end
 
-  # POST /books
-  def create
-    @book = Book.new(book_params)
-    @book.user = @current_user
-    if @book.save
-      render json: @book, status: :created, location: @book
-    else
-      render json: @book.errors, status: :unprocessable_entity
-    end
-  end
+  # # POST /books
+  # def create
+  #   @book = Book.new(book_params)
+  #   # @book.user = @current_user
+  #   if @book.save
+  #     render json: @book, status: :created, location: @book
+  #   else
+  #     render json: @book.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # PATCH/PUT /books/1
-  def update
-    if @book.update(book_params)
-      render json: @book
-    else
-      render json: @book.errors, status: :unprocessable_entity
-    end
-  end
+  # # PATCH/PUT /books/1
+  # def update
+  #   if @book.update(book_params)
+  #     render json: @book
+  #   else
+  #     render json: @book.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # DELETE /books/1
-  def destroy
-    @book.destroy
-  end
+  # # DELETE /books/1
+  # def destroy
+  #   @book.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -45,8 +45,8 @@ class BooksController < ApplicationController
       @book = Book.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
-    def book_params
-      params.require(:book).permit(:title, :author, :genre, :release_date, :img_url, :synopsis)
-    end
+  #   # Only allow a list of trusted parameters through.
+  #   def book_params
+  #     params.require(:book).permit(:title, :author, :genre, :release_date, :img_url, :synopsis)
+  #   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -40,6 +40,10 @@ class ReviewsController < ApplicationController
     @review.destroy
   end
 
+  def get_all_book_reviews
+    @
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_review
@@ -50,4 +54,5 @@ class ReviewsController < ApplicationController
     def review_params
       params.require(:review).permit(:user_id, :book_id, :rating, :content)
     end
+
 end
